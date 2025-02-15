ee49daa5535590ce6a8975f98cf328cd6c988de2
Elie G <elyahou.hadass@gmail.com>
2025-02-15 21:50:27 +0200

    Bump version to 0.3.0
    Update the project version in build.gradle.kts from 0.2.0 to 0.3.0. This indicates the release of new changes or improvements in the media player module.

--- mediaplayer/build.gradle.kts
+++ mediaplayer/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 group = "io.github.kdroidfilter.composemediaplayer"
-version = "0.2.0"
+version = "0.3.0"
 
 val osName = System.getProperty("os.name").lowercase(Locale.getDefault())
 val osArch = System.getProperty("os.arch").lowercase(Locale.getDefault())
